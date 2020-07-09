69c8794210245cfa178be067b450e938ecaa7120
Lucas Mezuraro <lucaspmmezuraro@gmail.com>
2020-07-09 14:37:59 -0300

    add mongodb container connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://db:27017/pizz_finder_api', {
+    authSource: 'admin',
+    user: 'root',
+    pass: 'rootpassword'
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
